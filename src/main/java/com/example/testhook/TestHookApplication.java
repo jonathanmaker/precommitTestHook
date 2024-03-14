d0969251cce0cde5cdf49f6fe3e9b541b2c6a08e
jonathanmaker <jonathantbm2015@gmail.com>
2024-03-14 09:22:24 -0300

    initial commit

---
+++ src/main/java/com/example/testhook/TestHookApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHookApplication.class, args);
+    }
+
+}
